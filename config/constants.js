a6818224f7041f8d2670ff9b74ba048fc2a03301
chiragadhvaryu <chirag@astrics.io>
2021-02-01 22:17:36 +0530

    Changes In File

--- config/constants.js
+++ config/constants.js
@@ -2,7 +2,7 @@ module.exports = {
     JWT_SECRET :"JWT_SECRET",
     API_URL : "https://datatbale-api.herokuapp.com:5000/",
     config: {
-        host: '3.223.187.194',
+        host: '3.223.187.194:/var/run/mysqld/mysql.sock',
         port: "3306",
         user: 'temp_user',
         password: '6dPhe@48',
