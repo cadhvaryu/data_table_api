fa73beadb4f330f1f1840ee9aae7f5fb51897a95
chiragadhvaryu <chirag@astrics.io>
2021-02-01 12:55:23 +0530

    Changes In Database File

--- config/constants.js
+++ config/constants.js
@@ -2,10 +2,10 @@ module.exports = {
     JWT_SECRET :"JWT_SECRET",
     API_URL : "http://localhost:5000/",
     config: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'db_templates', //database name
+        host: 'eu-cdbr-west-03.cleardb.net',
+        user: 'b61a43272ad92a',
+        password: '97a29601',
+        database: 'heroku_d6ceef3009c89e3', //database name
         multipleStatements: true,
         whichdb: "LOCAL"
     },
